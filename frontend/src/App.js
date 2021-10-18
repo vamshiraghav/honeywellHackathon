e17eb15f1e2dbc03544d3cb2232da8f7690d7fc2
vamshiraghav <vamshiraghava@gmail.com>
2021-10-18 18:26:09 +0530

    initial frontend react setup

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+import React from 'react';
+
+function App() {
+  return (    <div className="App">
+    HoneyWell Hackathon
+    </div>
+  );
+}
+
+export default App;
